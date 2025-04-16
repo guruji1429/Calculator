0ea7b8cf61eccded0a05d0b7f2ea0b40bd499647
Gurmeet <singhgurmeet1429@gmail.com>
2025-04-16 12:17:04 +0000

    Completed

---
+++ static.js
@@ -0,0 +1,22 @@
+let string = " ";
+let buttons = document.querySelectorAll('.Button');
+Array.from(buttons).forEach((Button)=>{
+    Button.addEventListener('click',(e)  =>{
+        if(e.target.innerHTML == '='){
+            string=eval(string);
+            document.querySelector('input').value=string;
+
+        }
+        else if(e.target.innerHTML == 'C'){
+            string="";
+            document.querySelector('input').value=string;
+
+        }
+        
+        else{
+        console.log(e.target)
+        string=string + e.target.innerHTML;
+        document.querySelector('input').value=string;
+        }
+    })
+})
